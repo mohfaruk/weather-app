7394d6800bff6eb0b5647451f6ebe9b5814673c3
Mohamad Faruk <mohamadfaruk.uk~gmail.com>
2021-11-27 16:38:45 +0000

    updated version

--- app.js
+++ app.js
@@ -1,28 +1,54 @@
-//inst classes//
+//DOM Variables
+const input = document.querySelector(".search-bar");
+const errorBox = document.querySelector(".error");
+const main = document.querySelector("#name");
+const temperature = document.querySelector(".temperature");
+const description = document.querySelector(".description");
+const icon = document.querySelector(".icon");
+const button = document.querySelector(".submit");
 
-const ft = new Fetch();
-const ui = new UI();
+//Clears text from searchbar and error box
+function clearSearch() {
+  input.value = " ";
+  errorBox.innerHTML = " ";
+}
 
-//DOM variables
+//Displays error message
+function errorMessage(error) {
+  errorBox.innerHTML = "City not found, try again...";
+  console.log(`Something went wrong... ${error}`);
+}
 
-const search = document.getElementById("searchUser");
-const button = document.getElementById("submit");
-button.addEventListener("click", () => {
-  const currentVal = search.value;
+//Fetches weather data
+function getWeather() {
+  const unit = "metric"; //Temperature unit for url
+  //Fetches url
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=5f833083abdac8c9b95d87a3d3cddf48&units=${unit}`
+  )
+    .then(response => response.json())
+    .then(data => {
+      let tempValue = data.main.temp.toFixed() + " °C";
+      let nameValue = data.name;
+      let descValue = data.weather[0].description;
+      let iconValue = data.weather[0].icon;
 
-  ft.getCurrent(currentVal).then(data => {
-    //call a UI method//
-    ui.populateUI(data);
-    //call saveToLS
-    ui.saveToLS(data);
-  });
-});
+      //Displays temperature info to DOM
+      main.innerHTML = nameValue;
+      temperature.innerHTML = tempValue;
+      icon.src = `http://openweathermap.org/img/wn/${iconValue}@2x.png`;
+      description.innerHTML = `Weather conditions:  ${descValue}`;
 
-//event listener for local storage
+      clearSearch(); //Runs function when search has been made
+    })
 
-window.addEventListener("DOMContentLoaded", () => {
-  const dataSaved = ui.getFromLS();
-  ui.populateUI(dataSaved);
-});
+    .catch(error => errorMessage(error)); //Runs error message
+}
 
-//https://www.youtube.com/watch?v=f__x1VofV2Q&ab_channel=CodeCreative
+//Execute search when user hits enter
+input.onkeydown = event => {
+  if (event.key == "Enter") {
+    getWeather();
+  }
+};
+button.addEventListener("click", getWeather); //Execute search when user clicks button
